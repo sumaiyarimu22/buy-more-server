bda7d8526f768d8dff1e36755836ddc2397f78d2
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2023-11-30 23:31:53 +0600

    user get and update cntrl added

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,6 @@ export const createUser = asyncHandler(async (req, res) => {
   //hash password
   const salt = await bcrypt.genSalt(10);
   const hasdedPassWord = await bcrypt.hash(password, salt);
-
   const newUser = new User({ username, email, password: hasdedPassWord });
 
   try {
@@ -65,4 +64,51 @@ export const logoutCurrentUser = asyncHandler(async (req, res) => {
     expires: new Date(0),
   });
   res.status(200).json({ message: "Logged out successfully" });
+});
+
+export const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+export const getCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hasdedPassWord = await bcrypt.hash(req.body.password, salt);
+      user.password = hasdedPassWord;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
